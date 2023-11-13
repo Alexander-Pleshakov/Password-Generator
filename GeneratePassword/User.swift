f680b325d81df1d6227459bf8922c6cab2404244
Pleshakov-Alexandr <sanya.pleshakov.95@mail.ru>
2023-11-14 01:57:48 +0300

    Realized password generator

--- GeneratePassword/User.swift
+++ GeneratePassword/User.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+public struct User {
+    public var id: String?
+    public var password: String?
+    
+    public mutating func generatePassword() {
+        let russianLetters = ["Ё", "Й", "Ц", "У", "К", "Е", "Н", "Г", "Ш", "Щ", "З", "Х", "Ъ", "Ф", "Ы", "В", "А", "П", "Р", "О", "Л", "Д", "Ж", "Э", "Я", "Ч", "С", "М", "И", "Т", "Ь", "Б", "Ю"]
+        let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        let symbols = ["!", "\"", "#", "$", "%", "&", "'", "(", ")", "*"]
+        let n = Int.random(in: 0...10)
+
+        let b1 = russianLetters[Int.random(in: 0..<33)]
+        let b2 = russianLetters[Int.random(in: 0..<33)]
+        let b3 = String((n * n) % 10)
+        let b4 = numbers[Int.random(in: 0..<10)]
+        let b5 = symbols[Int.random(in: 0..<10)]
+        let b6 = russianLetters[Int.random(in: 0..<33)].lowercased()
+
+        password = b1 + b2 + b3 + String(b4) + b5 + b6
+    }
+}
--- GeneratePassword/ViewController.swift
+++ GeneratePassword/ViewController.swift
@@ -8,11 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var generateButton: UIButton!
+    @IBOutlet weak var passwordTextView: UITextView!
+    @IBOutlet weak var statusInfoLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            view.endEditing(true)
+        }
+        
+        super.touchesBegan(touches, with: event)
+    }
+    
+    @IBAction func buttonGenerate(_ sender: Any) {
+        var user = User(id: idTextField.text)
+        user.generatePassword()
+        statusInfoLabel.text = "Сгенерированный пароль:"
+        if let password = user.password {
+            passwordTextView.text = password
+        } else {
+            passwordTextView.text = "Произошла ошибка генерации"
+        }
+        passwordTextView.backgroundColor = .systemGray6
+        
+        view.endEditing(true)
+    }
+    
 
 
 }
